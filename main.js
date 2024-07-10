14ba657a4d8e6625018ed4d4e65efbbb7af6e653
Ph0enixReign <157275588+Meesen11@users.noreply.github.com>
2024-07-10 20:59:30 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,10 @@
 let balance = 0;
 let income = 1; // доход с клика
 
-
-let title = document.createElement("h1");
-title.textContent= ` ${balance}`;
-
-let button = document.createElement("button");
-button.textContent = "Нажать";
+function dumplings_click() {
+  balance += income;
+  document.getElementById("balance_display").textContent = balance; // обновляем отображение баланса
+}
 
 
 
-
-
-button.onclick = function () {
-  balance+=income;
-  title.textContent = `${balance}`;
-};
-
-document.body.append(title);
-document.body.append(button);
